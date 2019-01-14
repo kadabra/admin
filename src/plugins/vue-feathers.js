2fecdb3685079caeeacfdfa2685b3081bbdd12e9
Patrick Keenan <sirberus.io@gmail.com>
2019-01-14 01:48:31 -0800

    fix

--- src/plugins/vue-feathers.js
+++ src/plugins/vue-feathers.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 import VueFeathers from '@vue-feathers/vue-feathers'
-import { feathers as feathersClient} from './feathers'
+import { default as feathersClient } from './feathers'
 
 Vue.use(VueFeathers, { feathersClient })
